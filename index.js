de24ccfc6279435e7e48db5f5998a7f755ca6aec
kiddynamo <njenan@gmail.com>
2015-10-03 13:31:30 -0700

    wrapping index in function

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
-var seperator = '_';
+(function () {
 
-function flatten(source, out, path) {
-    for (var k in source) {
-        if (typeof source[k] !== 'object') {
-            out[path === '' ? k : path + seperator + k] = source[k];
-        } else {
-            flatten(source[k], out, path === '' ? k : path + seperator + k);
+    var seperator = '_';
+
+    function flatten(source, out, path) {
+        for (var k in source) {
+            if (typeof source[k] !== 'object') {
+                out[path === '' ? k : path + seperator + k] = source[k];
+            } else {
+                flatten(source[k], out, path === '' ? k : path + seperator + k);
+            }
         }
     }
-}
 
-function unflatten(value, out, path) {
-    var current = path.shift();
+    function unflatten(value, out, path) {
+        var current = path.shift();
 
-    if (out[current] === undefined) {
-        if (path.length === 0) {
-            out[current] = value;
+        if (out[current] === undefined) {
+            if (path.length === 0) {
+                out[current] = value;
+            } else {
+                out[current] = {};
+                unflatten(value, out[current], path);
+            }
         } else {
-            out[current] = {};
             unflatten(value, out[current], path);
         }
-    } else {
-        unflatten(value, out[current], path);
     }
-}
 
 
-module.exports = {
-    denormalize: function (obj) {
-        var out = {}
-        flatten(obj, out, '');
-        return out;
-    },
-    normalize: function (obj) {
-        var out = {};
+    module.exports = {
+        denormalize: function (obj) {
+            var out = {};
+            flatten(obj, out, '');
+            return out;
+        },
+        normalize: function (obj) {
+            var out = {};
 
-        for (var k in obj) {
-            var split = k.split(seperator);
-            unflatten(obj[k], out, split);
+            for (var k in obj) {
+                var split = k.split(seperator);
+                unflatten(obj[k], out, split);
+            }
+
+            return out;
         }
-
-        return out;
-    }
-};
+    };
+})();
